38c2b3933dbebb72d4f49d54551cbe83c51d20bd
Ilia Surikov <surikovis89@gmail.com>
2024-01-08 22:52:25 +0300

    Initial commit

---
+++ Source/CppBase/Private/CppBaseActor.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppBaseActor.h"
+#include "Engine/Engine.h"
+
+std::atomic<int32_t> ACppBaseActor::instanceID = 0;
+
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerName = GetClass()->GetName();
+	PlayerName += "_";
+	PlayerName.AppendInt(instanceID++);
+	EnemyNum = 0;
+	IsAlive = true;
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+	showInformation();
+	
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ACppBaseActor::showInformation()
+{
+	UE_LOG(LogTemp, Display, TEXT("ID Name: %s"), *GetName());
+	UE_LOG(LogTemp, Display, TEXT("Actor Label: %s"), *GetActorLabel());
+	UE_LOG(LogTemp, Display, TEXT("Имя игрока: %s"), *PlayerName);
+	UE_LOG(LogTemp, Display, TEXT("Кол-во врагов: %i"), EnemyNum);
+	UE_LOG(LogTemp, Display, TEXT("Жив: %i"), IsAlive);
+}
+
---
+++ Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <atomic>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	FString PlayerName;
+
+	UPROPERTY(EditInstanceOnly)
+	int EnemyNum;
+
+	UPROPERTY(EditInstanceOnly)
+	bool IsAlive;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void showInformation();	
+	static std::atomic<int32_t> instanceID;
+};
+
+
+
